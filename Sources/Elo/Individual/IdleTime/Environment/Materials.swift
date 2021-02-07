471a78501dfa54f1b5be8a6faa4cc4e8d09c53b4
Paper <fredchowdz@gmail.com>
2021-02-07 19:41:55 +0800

    .

--- Sources/Elo/Individual/IdleTime/Environment/Materials.swift
+++ Sources/Elo/Individual/IdleTime/Environment/Materials.swift
@@ -3,6 +3,7 @@ import Elo_Itself
 struct Materials: Has_Criterias {
     var criterias: [Any] {
         [
+            "Flash",
             "Authentic",
             "First Hand",
             "Predecessor",
