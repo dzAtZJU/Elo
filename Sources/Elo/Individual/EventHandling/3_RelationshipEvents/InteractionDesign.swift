d5813529ed06f26acae32ead43f9ebc88efc4b88
Paper <fredchowdz@gmail.com>
2021-02-07 19:59:22 +0800

    .

--- Sources/Elo/Individual/EventHandling/3_RelationshipEvents/InteractionDesign.swift
+++
@@ -1,3 +0,0 @@
-struct InteractionDesign {
-//    bear://x-callback-url/open-note?id=A84257A1-CE86-425A-B263-97DF0DA11A7D-436-000065D4B49A99DE
-}
--- Sources/Elo/Individual/EventHandling/UserEvents/RelationshipEvents/InteractionDesign.swift
+++ Sources/Elo/Individual/EventHandling/UserEvents/RelationshipEvents/InteractionDesign.swift
@@ -1,5 +1,5 @@
 struct InteractionDesign {
-    
+    //    bear://x-callback-url/open-note?id=A84257A1-CE86-425A-B263-97DF0DA11A7D-436-000065D4B49A99DE
 }
 
 // 大家都很忙很有目的性，会在礼貌和谐的氛围中，对对方做出判断，哪怕是一个小点不满意也会分手
